2fdbb34851826176c1b31f14d47d4722fb37d50c
Foy <ryocr000@gmail.com>
2024-02-27 09:42:14 -0600

    Add Challenge java class
    Adding first version of Challenge using lombok for boilerplate

---
+++ src/main/java/microservices/foytest/multiplication/challenge/Challenge.java
@@ -0,0 +1,16 @@
+package microservices.foytest.multiplication.challenge;
+
+import lombok.*;
+
+/**
+ * This class represents a Challenge to solve a Multiplication (a * b)
+ * It uses lombok to produce all the boilerplate required via annotations
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Challenge {
+    private int factorA;
+    private int factorB;
+}
