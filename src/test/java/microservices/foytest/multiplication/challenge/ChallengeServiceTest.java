8882bdb1f8540353e634970ede642b52f8d99089
Foy <ryocr000@gmail.com>
2024-02-27 12:06:36 -0600

    Add Challenge Service Test
    Adding tests for right and wrong answers to the challenge, both failed because implement the corresponding logic is required

---
+++ src/test/java/microservices/foytest/multiplication/challenge/ChallengeServiceTest.java
@@ -0,0 +1,39 @@
+package microservices.foytest.multiplication.challenge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class ChallengeServiceTest {
+    private ChallengeService challengeService;
+
+    @BeforeEach
+    public void setUp() {
+        challengeService = new ChallengeServiceImpl();
+    }
+
+    @Test
+    public void checkCorrectAttemptTest() {
+        // given
+        ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "Foy", 3000);
+
+        // when
+        ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
+
+        // then
+        then(resultAttempt.isCorrect()).isTrue();
+    }
+
+    @Test
+    public void checkWrongAttemptTest() {
+        // given
+        ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "Foy", 5000);
+
+        //when
+        ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
+
+        //Then
+        then(resultAttempt.isCorrect()).isFalse();
+    }
+}
