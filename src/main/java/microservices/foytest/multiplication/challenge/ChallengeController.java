fbaeac0224208844572c756a1329fa3010e66ac0
Foy <ryocr000@gmail.com>
2024-02-27 13:45:51 -0600

    Add ChallengeController
    Adding an REST API controller required to get random challenges

---
+++ src/main/java/microservices/foytest/multiplication/challenge/ChallengeController.java
@@ -0,0 +1,26 @@
+package microservices.foytest.multiplication.challenge;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This class implements a REST API to get random challenges
+ */
+@Slf4j  //logging, enables the log object
+@RequiredArgsConstructor //create a constructor with the variables private and final (ChallengeGeneratorService)
+//Spring uses dependency injection so the only candidate for ChallengeGeneratorService is ChallengeGeneratorServiceImpl
+@RestController  //rest controller annotation, the response is JSON by default
+@RequestMapping("/challenges") //map API context
+public class ChallengeController {
+    private final ChallengeGeneratorService challengeGeneratorService;
+
+    @GetMapping("/random")
+    Challenge getRandomChallenge() {
+        Challenge challenge = challengeGeneratorService.randomChallenge();
+        log.info("Random challenge generated: {}", challenge);
+        return challenge;
+    }
+}
