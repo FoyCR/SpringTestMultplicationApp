1a0c763ff72b32763e2a079a8b978e2dd2e98637
Foy <ryocr000@gmail.com>
2024-02-27 11:15:16 -0600

    Update Challenge generation logic
    Adding the logic to the ChallengeGeneratorServiceImpl to pass the test

--- src/main/java/microservices/foytest/multiplication/challenge/ChallengeGeneratorServiceImpl.java
+++ src/main/java/microservices/foytest/multiplication/challenge/ChallengeGeneratorServiceImpl.java
@@ -1,21 +1,30 @@
 package microservices.foytest.multiplication.challenge;
 
 import org.springframework.stereotype.Service;
+
 import java.util.Random;
 
 @Service
 public class ChallengeGeneratorServiceImpl implements ChallengeGeneratorService {
+    private final static int MINIMUM_FACTOR = 11;
+    private final static int MAXIMUM_FACTOR = 100;
     private final Random random;
+
 
     ChallengeGeneratorServiceImpl() {
         this.random = new Random();
     }
-    protected ChallengeGeneratorServiceImpl(final Random random)
-    {
+
+    protected ChallengeGeneratorServiceImpl(final Random random) {
         this.random = random;
     }
+
     @Override
     public Challenge randomChallenge() {
-        return null;
+        return new Challenge(nextFactor(), nextFactor());
+    }
+
+    private int nextFactor() {
+        return random.nextInt(MAXIMUM_FACTOR - MINIMUM_FACTOR) + MINIMUM_FACTOR;
     }
 }
