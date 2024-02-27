cfe29d87112db98d5146f3bc36bae67c354e1fb4
Foy <ryocr000@gmail.com>
2024-02-27 12:21:16 -0600

    Update logic in the Challenge Service
    Adding logic to verify the attempt, with this the tests for the service will pass.

--- src/main/java/microservices/foytest/multiplication/challenge/ChallengeServiceImpl.java
+++ src/main/java/microservices/foytest/multiplication/challenge/ChallengeServiceImpl.java
@@ -1,5 +1,6 @@
 package microservices.foytest.multiplication.challenge;
 
+import microservices.foytest.multiplication.user.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,6 +8,21 @@ public class ChallengeServiceImpl implements ChallengeService {
 
     @Override
     public ChallengeAttempt verifyAttempt(ChallengeAttemptDTO attemptDTO) {
-        return null;
+        //check if the attempt is correct
+        boolean isCorrect = attemptDTO.getAnswer() == attemptDTO.getFactorA() * attemptDTO.getFactorB();
+
+        //We do not use identifier at this point
+        User user = new User(null, attemptDTO.getUserAlias());
+
+        //Builds the domain object. Null id for now.
+        ChallengeAttempt verifiedAttempt = new ChallengeAttempt(
+                null,
+                user.getId(),
+                attemptDTO.getFactorA(),
+                attemptDTO.getFactorB(),
+                attemptDTO.getAnswer(),
+                isCorrect);
+
+        return verifiedAttempt;
     }
 }
