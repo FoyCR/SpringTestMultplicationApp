704c6271f3e35aab2a0f694be8f1aa53158f635e
Foy <ryocr000@gmail.com>
2024-03-11 11:31:13 -0600

    add UserRepository

---
+++ src/main/java/microservices/foytest/multiplication/user/UserRepository.java
@@ -0,0 +1,14 @@
+package microservices.foytest.multiplication.user;
+
+import org.apache.catalina.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+// You don’t need to implement these interfaces. You don’t even need to add the Spring’s @Repository annotation.
+// Spring, using the Data module, will find interfaces extending the base ones and will inject beans that implement
+// the desired behavior
+public interface UserRepository extends CrudRepository<User, Long> {
+    // Return a user wrapped in a Java Optional<T>
+    Optional<User> finByAlias(final String alias);
+}
