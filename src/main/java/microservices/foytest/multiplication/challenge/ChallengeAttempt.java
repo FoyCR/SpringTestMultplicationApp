da8f170b0f294aa15f214348b43c6438dfa28169
Foy <ryocr000@gmail.com>
2024-03-11 09:50:29 -0600

    update ChallengeAttempt as Data Entity
    Adding annotations to handle as an Database Entity

--- src/main/java/microservices/foytest/multiplication/challenge/ChallengeAttempt.java
+++ src/main/java/microservices/foytest/multiplication/challenge/ChallengeAttempt.java
@@ -2,19 +2,31 @@ package microservices.foytest.multiplication.challenge;
 
 import lombok.*;
 import microservices.foytest.multiplication.user.User;
+import jakarta.persistence.*;
+import microservices.foytest.multiplication.user.Users;
 
 /**
  * Identifies the attempt from a {@link User} to solve a Challenge
  * It stored the factors of the challenge, because the challenge is created 'on the fly'
  * It uses lombok to produce all the boilerplate required via annotations
  */
-@Getter
-@ToString
-@EqualsAndHashCode
+@Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeAttempt {
+    @Id
+    @GeneratedValue
     private Long id;
-    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY) //one use can have many attempts. If you set it to EAGER, the user data gets
+    // collected with the attempt. With LAZY, the query to retrieve those fields will be executed only when you try to
+    // access them
+    @JoinColumn(name = "USER_ID") //This annotation makes Hibernate link both tables with a join column, It will create
+    // a new column in the CHALLENGE_ATTEMPT table called USER_ID which will store the reference to the ID record of the
+    // corresponding user in the USER table.
+    private Users users; //embedded entity type: users
+
     private int factorA;
     private int factorB;
     private int answerAttempt;
