cf1434ee6a8efcbeff385053e2716ddcfac7cb7a
Foy <ryocr000@gmail.com>
2024-03-13 15:29:29 -0600

    Update ChallengeComponent
    to add the ListAttemptsComponent and pass the attempts array plus minor refactoring

--- challenges-frontend/src/Components/ChallengeComponent.js
+++ challenges-frontend/src/Components/ChallengeComponent.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import ApiClient  from "../services/ApiClient";
+import ListAttemptsComponent from "./ListAttemptsComponent";
 
 class ChallengeComponent extends React.Component {
     constructor(props) {
@@ -9,7 +10,8 @@ class ChallengeComponent extends React.Component {
             b: '', 
             user: '', 
             message: '',
-            answer: 0 
+            answer: 0,
+            lastAttempts: [] //this one will hold the last attempts array
         };
         this.handleSubmitAttempt = this.handleSubmitAttempt.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -18,6 +20,9 @@ class ChallengeComponent extends React.Component {
     //The componentDidMount function is a lifecycle method that you can implement
     // in React to execute logic right after the component is rendered the first time.
     componentDidMount(): void {
+        this.refreshChallenge();
+    }
+    refreshChallenge(){
         ApiClient.challenge().then (
             res => {
                 if (res.ok) {
@@ -57,6 +62,8 @@ class ChallengeComponent extends React.Component {
                     } else {
                         this.updateMessage("Sorry, Your answer is wrong, but keep playing!");
                     }
+                    this.updateLastAttempts(this.state.user);
+                    this.refreshChallenge();
 
                 });
             }else {
@@ -71,14 +78,30 @@ class ChallengeComponent extends React.Component {
         });
     }
 
+    updateLastAttempts(userAlias: string) {
+        ApiClient.getAttempts(userAlias).then(res => {
+            if (res.ok) {
+                let attempts: Attempt[] = [];
+                res.json().then(data => {
+                    data.forEach(item => {
+                        attempts.push(item);
+                    });
+                    this.setState({
+                        lastAttempts:attempts
+                    });
+                })
+            }
+        })
+    }
+
     render () {
         return (
-            <div>
-                <div>
-                    <h2>Welcome to Foy's Mental Multiplication Challenge: </h2>
-                    <h3>Your new challenge is: </h3>
-                    <h1>{this.state.a} x {this.state.b}</h1>
-                </div>
+            <div className="display-column">
+               
+                <h2>Welcome to Foy's Mental Multiplication Challenge: </h2>
+                <h3>Your new challenge is: </h3>
+                <div className="challenge">{this.state.a} x {this.state.b}</div>
+                <br/>
                 <form onSubmit={this.handleSubmitAttempt}>
                     <label>
                         Your Alias: <input type = "text" maxLength="23" name="user" value={this.state.user} onChange={this.handleChange} />
@@ -91,6 +114,7 @@ class ChallengeComponent extends React.Component {
                     <input type="submit" value="Send Attempt"></input>
                 </form>
                 <h4>{this.state.message}</h4>
+                {this.state.lastAttempts.length > 0 && <ListAttemptsComponent lastAttempts={this.state.lastAttempts}/>}
             </div>
         );
     }
