da583de9a6c7cc5b26d7de951ef0ec49c0410a0c
Foy <ryocr000@gmail.com>
2024-03-13 15:26:35 -0600

    Update ApiClient
    adding getAttempts method get the last attempts for an user alias

--- challenges-frontend/src/Services/ApiClient.js
+++ challenges-frontend/src/Services/ApiClient.js
@@ -2,6 +2,7 @@ class ApiClient {
     static SERVER_URL = 'http://localhost:8080'; //base URL for the endpoints
     static GET_CHALLENGE = '/challenges/random'; //endpoint for GET randomly generated challenge
     static POST_ATTEMPT = '/attempts'; //endpoint for POST the attempt to solve a challenge
+    static GET_ATTEMPTS_BY_ALIAS ='/attempts?alias='; // endpoint for GET the attempts for user's alias
 
     static challenge(): Promise<Response> {
         return fetch(`${ApiClient.SERVER_URL}${ApiClient.GET_CHALLENGE}`); //fetching data from the REST API
@@ -21,6 +22,10 @@ class ApiClient {
                 }
             )
         });
+    }
+
+    static getAttempts(userAlias: string): Promise<Response> {
+        return fetch(`${ApiClient.SERVER_URL}${ApiClient.GET_ATTEMPTS_BY_ALIAS}${userAlias}`);
     }
 }
 export default ApiClient;
