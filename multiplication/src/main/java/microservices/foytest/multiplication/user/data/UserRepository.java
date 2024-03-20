27817ef9addf3de1bb98bb0c5ad655f66dedfbc9
Foy <ryocr000@gmail.com>
2024-03-20 13:43:13 -0600

    Update UserRepository
    to the findAllByIdIn method

--- multiplication/src/main/java/microservices/foytest/multiplication/user/data/UserRepository.java
+++ multiplication/src/main/java/microservices/foytest/multiplication/user/data/UserRepository.java
@@ -1,13 +1,25 @@
 package microservices.foytest.multiplication.user.data;
 
+import microservices.foytest.multiplication.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 // You don’t need to implement these interfaces. You don’t even need to add the Spring’s @Repository annotation.
 // Spring, using the Data module, will find interfaces extending the base ones and will inject beans that implement
 // the desired behavior
 public interface UserRepository extends CrudRepository<User, Long> {
-    // Return a user wrapped in a Java Optional<T>
+    /**
+     * Return a user wrapped in a Java Optional<T>
+     */
     Optional<User> findByAlias(final String alias);
+
+    /**
+     * Retrieve a list of users by their ids
+     *
+     * @param ids List of user ids
+     * @return List of users
+     */
+    List<User> findAllByIdIn(final List<Long> ids);
 }
