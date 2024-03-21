e8692b20216a650fa3680cfdae8eed1586f50984
Foy <ryocr000@gmail.com>
2024-03-21 10:15:18 -0600

    Update Pom.xml and JsonConfiguration
    Due an error at runtime I use the same hibernate package that the multiplication service

--- gamification/src/main/java/microservices/foy/gamification/configuration/JsonConfiguration.java
+++ gamification/src/main/java/microservices/foy/gamification/configuration/JsonConfiguration.java
@@ -1,14 +1,16 @@
 package microservices.foy.gamification.configuration;
 
 import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// This apply Hibernate5JakartaModule to avoid serialize errors with empty Bean without force the retrieve from db
 public class JsonConfiguration {
     @Bean
     public Module hibernateModule() {
-        return new Hibernate5Module();
+        return new Hibernate5JakartaModule();
     }
+
 }
