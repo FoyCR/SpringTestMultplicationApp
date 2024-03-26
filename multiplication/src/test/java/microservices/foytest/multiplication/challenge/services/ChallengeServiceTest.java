293bf18e962d2970e6336f2150b82aa9119db888
Foy <ryocr000@gmail.com>
2024-03-26 10:54:38 -0600

    update ChallengeServiceTest
    to test the publisher method instead of the REST API Call

--- multiplication/src/test/java/microservices/foytest/multiplication/challenge/services/ChallengeServiceTest.java
+++ multiplication/src/test/java/microservices/foytest/multiplication/challenge/services/ChallengeServiceTest.java
@@ -3,7 +3,8 @@ package microservices.foytest.multiplication.challenge.services;
 import microservices.foytest.multiplication.challenge.data.ChallengeAttemptRepository;
 import microservices.foytest.multiplication.challenge.domain.ChallengeAttempt;
 import microservices.foytest.multiplication.challenge.dto.ChallengeAttemptDTO;
-import microservices.foytest.multiplication.clients.GamificationServiceClient;
+import microservices.foytest.multiplication.challenge.publishers.AttemptEventPublisher;
+// import microservices.foytest.multiplication.clients.GamificationServiceClient;
 import microservices.foytest.multiplication.user.domain.User;
 import microservices.foytest.multiplication.user.data.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,11 +35,13 @@ public class ChallengeServiceTest {
     private ChallengeAttemptRepository attemptRepository;
 
     @Mock
-    private GamificationServiceClient gameClient;
+    private AttemptEventPublisher attemptEventPublisher;
+    // private GamificationServiceClient gameClient; //Old version using a synchronous REST API call
 
     @BeforeEach
     public void setUp() {
-        challengeService = new ChallengeServiceImpl(userRepository, attemptRepository, gameClient);
+        challengeService = new ChallengeServiceImpl(userRepository, attemptRepository, attemptEventPublisher);
+        //challengeService = new ChallengeServiceImpl(userRepository, attemptRepository, gameClient);
     }
 
     @Test
@@ -46,7 +49,7 @@ public class ChallengeServiceTest {
         // given
         ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "Foy", 3000);
         given(attemptRepository.save(any())).will(returnsFirstArg());
-        given(gameClient.sendAttempt(any())).willReturn(true);
+        //given(gameClient.sendAttempt(any())).willReturn(true);
 
         // when
         ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
@@ -57,7 +60,8 @@ public class ChallengeServiceTest {
         //check for saving attempts
         verify(userRepository).save(new User("Foy"));
         verify(attemptRepository).save(resultAttempt);
-        verify(gameClient).sendAttempt(resultAttempt);
+        verify(attemptEventPublisher).AttemptVerified(resultAttempt);
+        //verify(gameClient).sendAttempt(resultAttempt);
     }
 
     @Test
@@ -65,7 +69,7 @@ public class ChallengeServiceTest {
         // given
         ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "Foy", 5000);
         given(attemptRepository.save(any())).will(returnsFirstArg());
-        given(gameClient.sendAttempt(any())).willReturn(true);
+        //given(gameClient.sendAttempt(any())).willReturn(true);
 
         //when
         ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
@@ -76,7 +80,8 @@ public class ChallengeServiceTest {
         //check for saving attempts
         verify(userRepository).save(new User("Foy"));
         verify(attemptRepository).save(resultAttempt);
-        verify(gameClient).sendAttempt(resultAttempt);
+        verify(attemptEventPublisher).AttemptVerified(resultAttempt);
+        //verify(gameClient).sendAttempt(resultAttempt);
     }
 
     @Test
@@ -85,7 +90,7 @@ public class ChallengeServiceTest {
         given(userRepository.findByAlias("Foy")).willReturn(Optional.of(existingUser));
         given(attemptRepository.save(any())).will(returnsFirstArg());
         ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "Foy", 5000);
-        given(gameClient.sendAttempt(any())).willReturn(true);
+        //given(gameClient.sendAttempt(any())).willReturn(true);
 
         //when
         ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
@@ -97,7 +102,8 @@ public class ChallengeServiceTest {
         then(resultAttempt.getUser()).isEqualTo(existingUser);
         verify(userRepository, never()).save(any());
         verify(attemptRepository).save(resultAttempt);
-        verify(gameClient).sendAttempt(resultAttempt);
+        verify(attemptEventPublisher).AttemptVerified(resultAttempt);
+        //verify(gameClient).sendAttempt(resultAttempt);
     }
 
     @Test
